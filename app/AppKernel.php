d604ac7204ed4ad3238b65d757345bc227d6efad
Bulava Eduard <bulavaeduard@gmail.com>
2017-07-29 17:06:12 +0300

    cors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Mcfedr\JsonFormBundle\McfedrJsonFormBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
